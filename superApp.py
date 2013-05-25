f8cfb3900fb48653d8b5f7a6a91c4a06b2f98043
Garrett Olson <garrettdolson@gmail.com>
2013-05-25 16:33:43 -0700

    Added jinja 2 stuff

--- superApp.py
+++ superApp.py
@@ -74,32 +74,24 @@ class MainPage(webapp2.RequestHandler):
             'url_linktext': url_linktext,
         }
 
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENVIRONMENT.get_template('mainpage.html')
+        self.response.write(template.render(template_values))
+
+class SignIn(webapp2.RequestHandler):
+
+ def get(self):		
+ 
+        template_values = {
+
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('signin.html')
         self.response.write(template.render(template_values))
 
 
-class Guestbook(webapp2.RequestHandler):
-
-    def post(self):
-        # We set the same parent key on the 'Greeting' to ensure each greeting
-        # is in the same entity group. Queries across the single entity group
-        # will be consistent. However, the write rate to a single entity group
-        # should be limited to ~1/second.
-        guestbook_name = self.request.get('guestbook_name',
-                                          DEFAULT_GUESTBOOK_NAME)
-        greeting = Greeting(parent=guestbook_key(guestbook_name))
-
-        if users.get_current_user():
-            greeting.author = users.get_current_user()
-
-        greeting.content = self.request.get('content')
-        greeting.put()
-
-        query_params = {'guestbook_name': guestbook_name}
-        self.redirect('/?' + urllib.urlencode(query_params))
 
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/sign', Guestbook),
-], debug=True)
+									('/', MainPage),
+								    ('/signin', SignIn),									
+									], debug=True)
